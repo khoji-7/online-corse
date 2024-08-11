73c5c35bb4529510b587f0706312451ee9beae43
khoji-7 <nematxonovh@gmail.com>
2024-08-11 21:31:43 +0500

    add sidebar active

--- src/components/sidebar/Sidebar.vue
+++ src/components/sidebar/Sidebar.vue
@@ -1,106 +1,119 @@
 <script setup lang="ts">
-import {ElIcon} from "element-plus";
-import {HomeFilled,Notebook,User,Message,Setting, UserFilled} from "@element-plus/icons-vue";
-
+import { ElIcon } from 'element-plus'
+import {
+  HomeFilled,
+  Notebook,
+  User,
+  Message,
+  Setting,
+  UserFilled
+} from '@element-plus/icons-vue'
 </script>
 
 <template>
-    <div class="sidebarParent">
-        <div class="container">
-            <img src="../../../public/ss.svg" alt="" class="logo" />
-            <div class="routeParent">
-                <router-link to="/" class="router">
-                    <el-icon >
-                        <HomeFilled/>
-                    </el-icon>
-                </router-link>
-                <router-link to="/teach" class="router">
-                    <el-icon><Notebook /></el-icon>
-                </router-link>
-                <router-link to="/user" class="router">
-                    <el-icon><User /></el-icon>
-                </router-link>
-                <router-link to="/messages" class="router">
-                    <el-icon><Message /></el-icon>
-                </router-link>
-                <router-link to="/settings" class="router">
-                    <el-icon><Setting /></el-icon>
-                </router-link>
-            </div>
+  <div class="sidebarParent">
+    <div class="container">
+      <img src="../../../public/ss.svg" alt="" class="logo" />
+      <div class="routeParent">
+        <router-link to="/" class="router" active-class="active-route">
+          <el-icon>
+            <HomeFilled />
+          </el-icon>
+        </router-link>
+        <router-link to="/teach" class="router" active-class="active-route">
+          <el-icon><Notebook /></el-icon>
+        </router-link>
+        <router-link to="/user" class="router" active-class="active-route">
+          <el-icon><User /></el-icon>
+        </router-link>
+        <router-link to="/messages" class="router" active-class="active-route">
+          <el-icon><Message /></el-icon>
+        </router-link>
+        <router-link to="/settings" class="router" active-class="active-route">
+          <el-icon><Setting /></el-icon>
+        </router-link>
+      </div>
 
-            <router-link to="/" class="router">
-                <el-icon><UserFilled /></el-icon>
-                 </router-link>
-        
-        </div>
+      <router-link to="/" class="router" active-class="active-route">
+        <el-icon><UserFilled /></el-icon>
+      </router-link>
     </div>
+  </div>
 </template>
 
 <style lang="scss" scoped>
 .container {
-    width: 130px;
-    height: 90vh;
-    background-color: black;
-    border-radius: 30px;
-    padding: 10px;
+  width: 130px;
+  height: 90vh;
+  background-color: black;
+  border-radius: 30px;
+  padding: 10px;
+  display: flex;
+  flex-direction: column;
+  color: white;
+  align-items: center;
+  justify-content: space-evenly;
+
+  .routeParent {
     display: flex;
     flex-direction: column;
-    color: white;
-    align-items: center;
     justify-content: space-evenly;
+    height: 40vh;
+  }
 
-    .routeParent {
-        display: flex;
-        flex-direction: column;
-        justify-content: space-evenly;
-        height: 40vh;
+  .router {
+    width: 35px;
+    height: 35px;
+    color: white;
+    :hover {
+      color: black;
+      background-color: white;
+      width: 40px;
+      height: 40px;
+      border-radius: 50%;
     }
+  }
+  .active-route {
+    color: black;
+    background-color: white;
+    border-radius: 50%;
+    padding: 5px;
+  }
 
-    .router {
-        width: 35px;
-        height: 35px;
-        color: white;
-        :hover {
-            color: black;
-            background-color: white;
-            transition: 0.2s ease-out;
-        }
-    }
-
-    .logo {
-        width: 100px;
-    }
+  .logo {
+    width: 100px;
+  }
 }
 
 @media (max-width: 768px) {
-    .container {
-        width: 100px;
-        height: 80vh;
+  .container {
+    width: 100px;
+    height: 80vh;
 
-        .router {
-            width: 30px;
-            height: 30px;
-        }
-
-        .logo {
-            width: 80px;
-        }
+    .router {
+      width: 30px;
+      height: 30px;
     }
+
+    .logo {
+      width: 80px;
+    }
+  }
 }
 
 @media (max-width: 480px) {
-    .container {
-        width: 60px;
-        height: 70vh;
+  .container {
+    width: 60px;
+    height: 70vh;
 
-        .router {
-            width: 25px;
-            height: 25px;
-        }
-
-        .logo {
-            width: 60px;
-        }
+    .router {
+      width: 25px;
+      height: 25px;
     }
+
+    .logo {
+      width: 60px;
+    }
+  }
 }
 </style>
